00f58458b46c9c1f2b1c97ac98c13758b3ed7dc6
NotNotEvan <evanjmaeslovett@gmail.com>
2023-12-30 13:51:48 -0500

    Add initial database schema and migration files

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,30 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as dotenv from 'dotenv';
+import * as schema from '../../../migrations/schema';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+
+dotenv.config({ path: '.env' });
+
+if (!process.env.DATABASE_URL) {
+  console.log('🔴 No database URL');
+}
+
+const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
+console.log('🔌 Connected to database');
+
+const db = drizzle(client, { schema });
+
+const migrateDb = async () => {
+  try {
+    console.log('🚀 Migrating database');
+    await migrate(db, { migrationsFolder: 'migrations' });
+    console.log('✅ Database migrated');
+  } catch (error) {
+    console.log('❌ Error migrating database:', error);
+  }
+};
+
+migrateDb();
+
+export default db;
