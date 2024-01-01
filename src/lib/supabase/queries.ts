b982a2104e150e49410c38ceeb83815a8f1179de
NotNotEvan <evanjmaeslovett@gmail.com>
2024-01-01 15:26:16 -0500

    Add supabase queries for user subscription status and workspace creation

---
+++ src/lib/supabase/queries.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { workspaces } from "../../../migrations/schema";
+import db from "./db";
+import { Subscription, Workspace } from "./supabase.types";
+
+export const getUserSubscriptionStatus = async (userId: string) => {
+  try {
+    const data = await db.query.subscriptions.findFirst({
+      where: (s, { eq }) => eq(s.userId, userId),
+    });
+    if (data) {
+      return { data: data as Subscription, error: null };
+    } else {
+      return { data: null, error: null };
+    }
+  } catch (error) {
+    return { data: null, error: `Error: ${error}` };
+  }
+};
+
+export const createWorkspace = async (workspace: Workspace) => {
+  try {
+    const response = await db.insert(workspaces).values(workspace);
+    return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: 'Error' };
+  }
+};
