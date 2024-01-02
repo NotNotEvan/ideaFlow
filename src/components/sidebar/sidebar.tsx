300f79470071aaa587231505368ff80adb4d3f33
NotNotEvan <evanjmaeslovett@gmail.com>
2024-01-01 19:45:47 -0500

    Add Sidebar component

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,37 @@
+import { getFolders, getUserSubscriptionStatus } from "@/lib/supabase/queries";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import React from "react";
+
+interface SidebarProps {
+  params: { workspaceId: string };
+  className?: string;
+}
+
+const Sidebar: React.FC<SidebarProps> = async ({ params, className }) => {
+  const supabase = createServerComponentClient({ cookies });
+  //user
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  //subscription
+  const { data: subscriptionData, error: subsriptionError } =
+    await getUserSubscriptionStatus(user.id);
+
+  //folders
+  const { data: workspaceFolderData, error: foldersError } = await getFolders(
+    params.workspaceId
+  );
+
+  //error
+  if (subsriptionError || foldersError) redirect("/dashboard");
+  
+  //get all different workspaces private collaborating shared
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
