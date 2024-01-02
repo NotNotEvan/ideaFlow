e263ce485dbc0cf0463010c7a98b8e7194a29b76
NotNotEvan <evanjmaeslovett@gmail.com>
2024-01-01 22:33:56 -0500

    Add WorkspaceDropdown component

---
+++ src/components/sidebar/workspace-dropdown.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useAppState } from "@/lib/providers/state-providers";
+import { workspace } from "@/lib/supabase/supabase.types";
+import React, { useEffect, useState } from "react";
+
+interface WorkspaceDropdownProps {
+  privateWorkspaces: workspace[] | [];
+  sharedWorkspaces: workspace[] | [];
+  collaboratingWorkspaces: workspace[] | [];
+  defaultValue: workspace | undefined;
+}
+
+const WorkspaceDropdown: React.FC<WorkspaceDropdownProps> = ({
+  privateWorkspaces,
+  sharedWorkspaces,
+  collaboratingWorkspaces,
+  defaultValue,
+}) => {
+  const { dispatch, state } = useAppState();
+  const [selectedOption, setSelectedOption] = useState(defaultValue);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (!state.workspaces.length) {
+      dispatch({
+        type: "SET_WORKSPACES",
+        payload: {
+          workspaces: [
+            ...privateWorkspaces,
+            ...sharedWorkspaces,
+            ...collaboratingWorkspaces,
+          ].map((workspace)=>({...workspace, folders: []}))
+        },
+      });
+    }
+  }, [privateWorkspaces, sharedWorkspaces, collaboratingWorkspaces]);
+
+  return <div>WorkspaceDropdown</div>;
+};
+
+export default WorkspaceDropdown;
